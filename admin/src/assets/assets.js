f649035366b363d695c30908d863906d6291e037
satyam kumar <03satyam05@gmail.com>
2025-07-09 23:23:40 +0530

    Update assets.js

--- admin/src/assets/assets.js
+++ admin/src/assets/assets.js
@@ -14,4 +14,4 @@ export const assets ={
     parcel_icon
 }
 
-export const url = 'https://food-delivery-backend-t1ku.onrender.com'
+export const url = 'https://food-delivery-backend-pmp0.onrender.com'
